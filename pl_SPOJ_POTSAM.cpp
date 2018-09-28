e20a8e79f81717fe1c38b853140bc3ac0c9505d1
Mytko Enko <deass@ukr.net>
2018-09-28 22:57:17 +0200

    Create pl_SPOJ_POTSAM.cpp

---
+++ pl_SPOJ_POTSAM.cpp
@@ -0,0 +1,17 @@
+/*
+ *  Problem name: POTSAM - Airplain (Samolot - EASY)
+ *  url: https://pl.spoj.com/problems/POTSAM/
+ *  Task: Multiply and add given values 1x2+3x4
+ */
+ #include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<a*b+c*d<<endl;
+ 
+    return 0;
+}
